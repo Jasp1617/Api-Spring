f5d862c91d327df707227af7aa8e0678ebf893b7
Jorge_Silva <112638015+Jasp1617@users.noreply.github.com>
2023-03-13 21:52:43 -0500

    Add files via upload

--- src/main/java/com/example/demo/ActorController.java
+++ src/main/java/com/example/demo/ActorController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
@@ -35,8 +36,10 @@ public Optional<Actor> findById(@PathVariable Integer id){
     return actorRepository.findById(id);
 }
 
+//guardar el empleado
+@CrossOrigin(origins = "http://localhost:4200")
 @PostMapping("/actor")
-public Actor findById(Actor actor){
+public Actor guardarActor(@RequestBody Actor actor){
     return actorRepository.save(actor);
 }
 
